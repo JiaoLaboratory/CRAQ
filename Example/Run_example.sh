865b806b36ed923cabd454bad53674f44e2f4c6c
JiaoLaboratory <65637958+JiaoLaboratory@users.noreply.github.com>
2023-02-11 11:50:13 +0800

    Delete Example directory

--- Example/Run_example.sh
+++
@@ -1,9 +0,0 @@
-
-#Get size file
-rm -rf SRout LRout runAQI_out
-perl ../src/fetch_size.pl Genome.fa >Genome.fa.size
-
-#Assessing
-perl ../bin/craq -g Genome.fa -z Genome.fa.size -lr SMS.bam  -sr NGS.bam -x map-ont 
-
-
