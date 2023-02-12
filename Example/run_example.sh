6f7a7d6dca8d660c3061b16bc212969e5ecdcd86
JiaoLaboratory <65637958+JiaoLaboratory@users.noreply.github.com>
2023-02-12 11:41:33 +0800

    second update

--- Example/run_example.sh
+++ Example/run_example.sh
@@ -1,5 +1,5 @@
 #Get genome size file
-rm -rf SRout LRout runAQI_out \
-perl ../src/fetch_size.pl Genome.fa >Genome.fa.size \
+rm -rf SRout LRout runAQI_out #
+perl ../src/fetch_size.pl Genome.fa >Genome.fa.size #
 #Assessing  
 perl ../bin/craq -g Genome.fa -z Genome.fa.size -lr SMS.bam  -sr NGS.bam -x map-ont  
