5614ffd7036cad48fac5e587615fc0e9182efb0f
JiaoLaboratory <65637958+JiaoLaboratory@users.noreply.github.com>
2023-05-16 21:43:04 +0800

    Update run_example.sh

--- Example/run_example.sh
+++ Example/run_example.sh
@@ -2,4 +2,4 @@
 rm -rf SRout LRout runAQI_out #
 perl ../src/fetch_size.pl Genome.fa >Genome.fa.size #
 #Assessing  
-perl ../bin/craq -g Genome.fa -z Genome.fa.size -lr SMS.bam  -sr NGS.bam -x map-ont  
+perl ../bin/craq -g Genome.fa -z Genome.fa.size -lr SMS_sort.bam  -sr NGS_sort.bam -rw 50000 -x map-ont  
