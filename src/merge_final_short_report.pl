530b05763912b394aafd3813b83d6488fc7575be
JiaoLaboratory <65637958+JiaoLaboratory@users.noreply.github.com>
2023-05-22 08:45:29 +0800

    Add files via upload

--- src/merge_final_short_report.pl
+++ src/merge_final_short_report.pl
@@ -13,17 +13,25 @@ my $avg_ler1=(split/\(/,$avg_ler)[0];
 $hr_hash{$chr}="$avg_ser1\t$avg_ler1";
 }
 
-print "Short Report:\n#Chr\tCovered.Rate\tavg.SHR\tavg.LHR\tavg.SER(S-AQI)\tavg.LER(L-AQI)\tWeight\tAQI\n";
+print "Short Report:\n#Chr\tCovered.Rate\tavg.SHR\tavg.LHR\tavg.SER(S-AQI)\tavg.LER(L-AQI)\tWeight\tAQI\tF1\n";
 open IN2,$er_report;
 while (<IN2>){
 chomp;
 next if/Short Report:/;
 next if/^#/;
 my ($chr,$cov_rate,$avg_ser, $avg_ler, $wei,$score)=(split/\s+/);
+my $avg_ser_score;
+my $avg_ler_score;
+   if($avg_ser=~/\S+\((\S+)\)/){   $avg_ser_score=$1;   }
+   if($avg_ler=~/\S+\((\S+)\)/){   $avg_ler_score=$1;   }
+
+my $f1_score=2*$avg_ser_score*$avg_ler_score/($avg_ser_score+$avg_ler_score+0.1);
+
+
 my  $hr_hash_value=$hr_hash{"$chr"};
 my ($avg_shr,$avg_lhr)=(split/\t/,$hr_hash_value);
 
-print "$chr\t$cov_rate\t$avg_shr\t$avg_lhr\t$avg_ser\t$avg_ler\t$wei\t$score\n";
+print "$chr\t$cov_rate\t$avg_shr\t$avg_lhr\t$avg_ser\t$avg_ler\t$score\t$wei\t$f1_score\n";
 
 }
 
