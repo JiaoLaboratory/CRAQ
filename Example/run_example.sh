05982b68a20567733bc04695a343f8a116eada75
JiaoLaboratory <65637958+JiaoLaboratory@users.noreply.github.com>
2023-05-26 16:48:21 +0800

    Update run_example.sh

--- Example/run_example.sh
+++ Example/run_example.sh
@@ -1,4 +1,4 @@
 ####
 rm -rf SRout LRout  #
 #Assessing  
-perl ../bin/craq -g Genome.fa -lr SMS_sort.bam  -sr NGS_sort.bam -rw 50000 -x map-ont  
+perl ../bin/craq -g Genome.fa -lr SMS_sort.bam  -sr NGS_sort.bam -rw 5000 -x map-ont -pl T 
