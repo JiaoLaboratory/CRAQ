7ffba29a8455ef03576972e96a4f275f26a7c0ef
JiaoLaboratory <65637958+JiaoLaboratory@users.noreply.github.com>
2023-06-22 14:51:07 +0800

    Add files via upload

--- src/runAQI_NGS.sh
+++ src/runAQI_NGS.sh
@@ -145,8 +145,8 @@ perl $src/final_short_report_minlen.pl runAQI_out/seq.H.stat  0.85 $report_minct
 perl $src/merge_final_short_report.pl runAQI_out/$name"_final.HR.Report.tmp" runAQI_out/$name"_final.ER.Report.tmp" >runAQI_out/$name"_final.Report.tmp"
 
 cat  $SR_coverRate runAQI_out/Gap_out/$name"_gap.out" > runAQI_out/all_putative_and_gapN.tmp
-perl $src/search_uncertain_region.pl runAQI_out/SRNonmap.loc.bed runAQI_out/all_putative_and_gapN.tmp  >runAQI_out/uncertain.bed
-perl $src/intergrate_uncertain.pl $ref_fa_size runAQI_out/uncertain.bed runAQI_out/$name"_final.Report.tmp" >runAQI_out/$name"_final.Report"
+perl $src/search_uncertain_region.pl runAQI_out/SRNonmap.loc.bed runAQI_out/all_putative_and_gapN.tmp  >runAQI_out/low_confidence.bed
+perl $src/intergrate_uncertain.pl $ref_fa_size runAQI_out/low_confidence.bed runAQI_out/$name"_final.Report.tmp" >runAQI_out/$name"_final.Report"
 
 mv runAQI_out/Gap_out/* runAQI_out/tmp_sequence.gapN 
 perl -alne  'print if( $F[3]>2 && ($F[3]/$F[4])> '$she_cutoff_right' && ($F[3]/$F[4])< '$srbk_cutoff')' SRout/SR_clip.coverRate >runAQI_out/locER_out/ambiguous.RE.RH
